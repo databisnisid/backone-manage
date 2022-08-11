c8a4daa8663f41b307f30a5f841480621fef437c
gkiserpong <gkiserpongit@gmail.com>
2022-08-12 00:12:21 +0700

    Snippet icon

--- controllers/wagtail_hooks.py
+++ controllers/wagtail_hooks.py
@@ -91,7 +91,7 @@ class ControllersAdmin(ModelAdmin):
 class UserControllersAdmin(ModelAdmin):
     model = UserControllers
     menu_label = 'User Controllers'  # ditch this to use verbose_name_plural from model
-    menu_icon = 'tag'  # change as required
+    menu_icon = 'snippet'  # change as required
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
     exclude_from_explorer = False # or True to exclude pages of this type from Wagtail's explorer view
     list_display = ('user', 'controller')
