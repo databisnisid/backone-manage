82752b5c86a1ffb474e5e55b2286e1a93da3fd1d
gkiserpong <gkiserpongit@gmail.com>
2022-08-20 22:22:39 +0700

    Add Synchronize Button in member

--- members/wagtail_hooks.py
+++ members/wagtail_hooks.py
@@ -1,10 +1,49 @@
 from wagtail.contrib.modeladmin.options import (
-    ModelAdmin, modeladmin_register, PermissionHelper)
+    ModelAdmin, modeladmin_register, ButtonHelper, PermissionHelper)
 from .models import Members, MemberPeers
 from crum import get_current_user
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel, FieldRowPanel, ObjectList
 from django.utils.translation import gettext as _
 from django.forms import HiddenInput
+from django.core.exceptions import ObjectDoesNotExist
+
+
+class MembersButtonHelper(ButtonHelper):
+
+    # Define classes for our button, here we can set an icon for example
+    #import_button_classnames = ["button-small", "icon", "icon-site"]
+    synchronize_classnames = ['button button-small button-primary']
+
+    def synchronize_button(self, obj):
+        if obj.configuration == '{}' and obj.is_authorized:
+            print('Synchronize Configuration ', obj.name)
+            obj.save()
+        if obj.peers.peers == '{}':
+            print('Synchronize Peers ', obj.name)
+            obj.peers.save()
+
+        # Define a label for our button
+        text = _('Synchronize')
+        return {
+            'url': self.url_helper.index_url, # Modify this to get correct action
+            'label': text,
+            'classname': self.finalise_classname(self.synchronize_classnames),
+            'title': text,
+        }
+
+    def get_buttons_for_obj(
+        self, obj, exclude=None, classnames_add=None, classnames_exclude=None
+    ):
+        """
+        This function is used to gather all available buttons.
+        We append our custom button to the btns list.
+        """
+        buttons = super().get_buttons_for_obj(
+            obj, exclude, classnames_add, classnames_exclude
+        )
+        if 'synchronize_button' not in (exclude or []):
+            buttons.append(self.synchronize_button(obj))
+        return buttons
 
 
 class MemberPeersPermissionHelper(PermissionHelper):
@@ -23,6 +62,7 @@ class MemberPeersPermissionHelper(PermissionHelper):
 
 class MembersAdmin(ModelAdmin):
     model = Members
+    button_helper_class = MembersButtonHelper
     inspect_view_enabled = True
     menu_label = 'Members'  # ditch this to use verbose_name_plural from model
     menu_icon = 'list-ul'  # change as required
@@ -38,8 +78,7 @@ class MembersAdmin(ModelAdmin):
         basic_panels = [
             MultiFieldPanel([FieldPanel('name'), FieldPanel('description')],
                             heading=_('Network Name and Description')),
-            MultiFieldPanel([FieldPanel('member_id', widget=HiddenInput()),
-                             FieldPanel('network')],
+            MultiFieldPanel([FieldPanel('member_id'), FieldPanel('network')],
                             heading=_('Member ID and Network')),
             # MultiFieldPanel([FieldPanel('is_authorized'), FieldPanel('ipaddress')],
             #                heading=_('Authorization and IP Address')),
