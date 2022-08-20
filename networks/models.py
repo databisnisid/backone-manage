9822e55cde03cdc65e85705d91af279405ad8931
gkiserpong <gkiserpongit@gmail.com>
2022-08-20 08:51:55 +0700

    DEV

--- networks/models.py
+++ networks/models.py
@@ -477,7 +477,7 @@ class NetworkRules(models.Model):
         file.close()
 
         shell = not settings.DEVELOPMENT
-        print(shell)
+        print(settings.DEVELOPMENT, shell)
 
         if self.rules_definition is not None:
             print('FILENAME ', filename_rule)
