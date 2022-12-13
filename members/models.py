b7d67412e00c8bd6b97f643bc2ab3ecb677992d0
gkiserpong <gkiserpongit@gmail.com>
2022-12-14 00:14:39 +0700

    Small IP Address

--- members/models.py
+++ members/models.py
@@ -214,6 +214,7 @@ class Members(models.Model):
             ipaddress_list = self.ipaddress.split(',')
             text = format_html('<br />'.join([str(p) for p in ipaddress_list]))
 
+        text = '<small>' + text + '</small>'
         return text
     list_ipaddress.short_description = _('IP Address')
 
