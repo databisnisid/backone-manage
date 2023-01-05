95df9774c22211ca41489ddd852bb7dcb4b44ab3
gkiserpong <gkiserpongit@gmail.com>
2023-01-05 17:37:26 +0700

    readable timedelta

--- config/utils.py
+++ config/utils.py
@@ -60,3 +60,15 @@ def get_cpu_usage(string, cpu):
     load_split = load.split(',')
     return float(load_split[0])/cpu*100, float(load_split[1])/cpu*100, float(load_split[2])/cpu*100
 
+def readable_timedelta(duration: timedelta):
+    data = {}
+    data['days'], remaining = divmod(duration.total_seconds(), 86_400)
+    data['hours'], remaining = divmod(remaining, 3_600)
+    data['minutes'], data['seconds'] = divmod(remaining, 60)
+
+    time_parts = [f'{round(value)} {name}' for name, value in data.items() if value > 0]
+    if time_parts:
+        return ' '.join(time_parts)
+    else:
+        return 'below 1 second'
+
