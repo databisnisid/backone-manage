94e77e5e8f6135ac4459e10e10fa68c0365aae9a
gkiserpong <gkiserpongit@gmail.com>
2023-01-11 15:43:50 +0700

    Split

--- members/models.py
+++ members/models.py
@@ -359,8 +359,8 @@ class Members(models.Model):
             first_line = '{} ({})'.format(model, num_core)
             second_line = serialnumber + ' - ' + release_version if serialnumber else release_version
             uptime_load = get_uptime_string(uptime)
-            print(uptime_load)
             uptime_split = uptime_load.split(', load average:')
+            print(uptime_split)
             uptime_string = uptime_split[0]
             if uptime:
                 load_1, load_5, load_15 = get_cpu_usage(uptime, num_core)
