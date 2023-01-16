709892b993ba27be37867fdb313db008e471a2b2
gkiserpong <gkiserpongit@gmail.com>
2023-01-16 16:56:23 +0700

    Get Routes

--- members/wagtail_hooks.py
+++ members/wagtail_hooks.py
@@ -188,7 +188,7 @@ class MembersAdmin(ModelAdmin):
     exclude_from_explorer = False # or True to exclude pages of this type from Wagtail's explorer view
     list_display = ('name', 'list_ipaddress',
                     'member_status', 'model_release',
-                    'list_peers', 'get_routes')
+                    'get_routes', 'list_peers')
     list_filter = ('network',)
     search_fields = ('name', 'member_id', 'ipaddress',
                      'mqtt__serialnumber', 'mqtt__model', 'mqtt__uptime',
