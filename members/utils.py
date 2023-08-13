5d92bdf3ce17fa8eda33080cd7e3c42fb276e7cd
gkiserpong <gkiserpongit@gmail.com>
2023-08-13 14:56:57 +0700

    Deauthorize Member base on offline_at

---
+++ members/utils.py
@@ -0,0 +1,22 @@
+from .models import Members
+from time import mktime
+from django.utils import timezone
+
+
+def deauthorize_member_offline_at():
+    '''
+    Deauthorize member which offline_at fields is set and less than current time
+    Run every minute at cronjob
+    '''
+    members = Members.objects.exclude(offline_at__isnull=True)
+
+    for member in members:
+        current_time = timezone.now()
+        time_delta = mktime(current_time.timetuple()) - mktime(member.offline_at.timetuple())
+
+        if time_delta > 0:
+            print('Deauthorize member {} - {} - {}'.format(member.name, member.member_id, member.offline_at))
+            member.is_authorized = False
+            member.save()
+
+
