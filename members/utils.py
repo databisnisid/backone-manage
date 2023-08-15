6c802461915ceac0e9b79ddab1d9811ec66c4af5
gkiserpong <gkiserpongit@gmail.com>
2023-08-15 15:40:04 +0700

    Print log for deauthorize_member_offline_at

--- members/utils.py
+++ members/utils.py
@@ -5,9 +5,12 @@ from django.utils import timezone
 
 def deauthorize_member_offline_at():
     '''
-    Deauthorize member which offline_at fields is set and less than current time
-    Run every minute at cronjob
+    Deauthorize member which offline_at fields is set 
+    and less than current time
+    Run every hour at cronjob
     '''
+
+    print('Start - deauthorize_member_offline_at() function')
     members = Members.objects.exclude(offline_at__isnull=True)
 
     for member in members:
@@ -19,4 +22,5 @@ def deauthorize_member_offline_at():
             member.is_authorized = False
             member.save()
 
+    print('Fin - deauthorize_member_offline_at() function')
 
