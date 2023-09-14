04c81aa90bda816722b6cf4d65619f16fbc9f898
gkiserpong <gkiserpongit@gmail.com>
2023-09-14 15:04:51 +0700

    is_operationaltime()

--- monitor/workers.py
+++ monitor/workers.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from config.utils import get_cpu_usage
 from mqtt.models import Mqtt
 from members.models import Members
-from monitor.models import MemberProblems, MonitorRules
+from monitor.models import MemberProblems, MonitorRules, OperationalTime
 from connectors.drivers import ping, ssh, mqtt
 
 
@@ -48,15 +48,48 @@ def is_problem(member, rule, is_online):
 
     return result
 
+def is_operationaltime(member):
+    result = False
+    try:
+        optime = OperationalTime.objects.get(network=member.network)
+        currtime = timezone.localtime()
+        dayname = currtime.strftime('%a')
+
+        print(dayname)
+
+        if dayname.lower() == 'mon' and optime.is_mon:
+            if currtime.hour > optime.mon_start and currtime.hour < optime.mon_end:
+                result = True
+        if dayname.lower() == 'tue' and optime.is_tue:
+            if currtime.hour > optime.tue_start and currtime.hour < optime.tue_end:
+                result = True
+        if dayname.lower() == 'wed' and optime.is_wed:
+            if currtime.hour > optime.wed_start and currtime.hour < optime.wed_end:
+                result = True
+        if dayname.lower() == 'thu' and optime.is_thu:
+            if currtime.hour > optime.thu_start and currtime.hour < optime.thu_end:
+                result = True
+        if dayname.lower() == 'fri' and optime.is_fri:
+            if currtime.hour > optime.fri_start and currtime.hour < optime.fri_end:
+                result = True
+        if dayname.lower() == 'sat' and optime.is_sat:
+            if currtime.hour > optime.sat_start and currtime.hour < optime.sat_end:
+                result = True
+        if dayname.lower() == 'sun' and optime.is_sun:
+            if currtime.hour > optime.sun_start and currtime.hour < optime.sun_end:
+                result = True
+
+    except ObjectDoesNotExist:
+        result = True
+    
+    return result
 
 
-#def check_members_vs_rules(member, mqtt):
 def check_members_vs_rules(member, is_online):
     result = []
     #rules = MonitorRules.objects.all()
     rules = MonitorRules.objects.filter(organization=member.user.organization)
     for rule in rules:
-        #if is_problem(member.mqtt, rule):
         if is_problem(member, rule, is_online):
             result.append(rule)
 
@@ -73,14 +106,11 @@ def monitor_members() :
 
     print("Start", end='')
     for member in members:
-        #if member.mqtt:
-        #mqtt = Mqtt.objects.get(member_id=member.member_id)
-        #mqtt = member.mqtt
         problems = []
         problems_offline = []
         is_solved = True
-        if member.ipaddress:
-            #if member.is_online() and ping.ping(member.ipaddress):
+
+        if member.ipaddress and is_operationaltime(member):
             if member.is_online() or ping.ping(member.ipaddress):
                 print('Checking {} ({})'. format(member.name, member.member_id))
                 print("online.", end='')
