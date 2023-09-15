d0685419b6308ce90aecd3beac73c36312cda8a0
gkiserpong <gkiserpongit@gmail.com>
2023-09-15 08:10:13 +0700

    Kebalik

--- members/models.py
+++ members/models.py
@@ -643,7 +643,7 @@ class Members(models.Model):
         text = ''
         if self.mqtt:
             rx_usage_value, tx_usage_value, total_usage_value = self.mqtt.get_quota_vnstat()
-            total_value, total_unit = calculate_bandwidth_unit(total_usage_value)
+            total_unit, total_value = calculate_bandwidth_unit(total_usage_value)
             text = str(round(total_value, 2)) + total_unit
 
         return text
