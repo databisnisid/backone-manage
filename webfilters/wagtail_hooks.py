184da625cc074e1c84e57c00492f69a3bd377b67
gkiserpong <gkiserpongit@gmail.com>
2024-01-09 15:07:27 +0700

    permission helper WebFiltersMembersList

--- webfilters/wagtail_hooks.py
+++ webfilters/wagtail_hooks.py
@@ -134,6 +134,7 @@ class WebFiltersMembersListAdmin(ModelAdmin):
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
     exclude_from_explorer = False # or True to exclude pages of this type from Wagtail's explorer view
     list_display = ('name')
+    permission_helper_class = WebfiltersMembersListPermissionHelper
     #search_fields = ('organization', 'uuid',)
 
     def get_queryset(self, request):
