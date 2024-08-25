f2e239f99124ca6b32f815bbd8e01c2b952abb9d
gkiserpong <gkiserpongit@gmail.com>
2024-08-25 17:20:33 +0700

    Remove try except

--- members/models.py
+++ members/models.py
@@ -276,22 +276,17 @@ class Members(models.Model):
 
     def list_ipaddress(self):
         #text = ''
-        text : string = self.get_hostname()
 
         if self.ipaddress is not None:
             ipaddress_list = self.ipaddress.split(',')
-            try: 
-                text += format_html('<br />'.join([str(p) for p in ipaddress_list]))
-            except TypeError:
-                text = format_html('<br />'.join([str(p) for p in ipaddress_list]))
-
+            text = format_html('<br />'.join([str(p) for p in ipaddress_list]))
 
         is_authorized = "icon-yes.svg" if self.is_authorized else "icon-no.svg"
         #return text
 
         # Get Hostname from MQTT
-        #if self.get_hostname():
-        #    text += format_html('<br />{}', self.get_hostname())
+        if self.get_hostname():
+            text = format_html('{}<br />{}', self.get_hostname(), text)
 
         if self.mobile_number_first is not None:
             text += format_html('<br />{}', self.mobile_number_first)
