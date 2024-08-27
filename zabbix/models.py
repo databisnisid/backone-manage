29e019096255e9b6a760a332c132676592a4c950
proitlab <dedy.sutanto@proit.co.id>
2024-08-27 13:09:05 +0700

    Zabbix Configs and django-qr-code==4.1.0

--- zabbix/models.py
+++ zabbix/models.py
@@ -3,9 +3,32 @@ from networks.models import Networks
 from django.utils.translation import gettext_lazy as _
 
 
+class ZabbixConfigs(models.Model):
+    name = models.CharField(_('Name'), max_length=50)
+    url = models.CharField(_('Zabbix API URL'), max_length=200)
+    token = models.CharField(_('Zabbix Token'), max_length=200)
+
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    class Meta:
+        db_table = 'zabbix_configs'
+        verbose_name = 'Zabbix Config'
+        verbose_name_plural = 'Zabbix Configs'
+
+    def __str__(self):
+        return '%s' % self.name
+
+
 class ZabbixNetworks(models.Model):
     name = models.CharField(_('Name'), max_length=50, default='ZabbixNet')
     networks = models.ManyToManyField(Networks)
+
+    config = models.ForeignKey(
+            ZabbixConfigs, 
+            on_delete=models.SET_NULL,
+            verbose_name=_('Zabbix Config'),
+            null=True)
 
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
--- zabbix/wagtail_hooks.py
+++ zabbix/wagtail_hooks.py
@@ -1,24 +1,28 @@
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin, ModelAdminGroup, PermissionHelper, modeladmin_register)
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel, FieldRowPanel
-from .models import ZabbixNetworks
+from .models import ZabbixConfigs, ZabbixNetworks
 from django.utils.translation import gettext_lazy as _
+
+
+class ZabbixConfigsAdmin(ModelAdmin):
+    model = ZabbixConfigs
+    menu_label = 'Zabbix Configs'  # ditch this to use verbose_name_plural from model
+    menu_icon = 'cog'  # change as required
+    add_to_settings_menu = True  # or True to add your model to the Settings sub-menu
+    exclude_from_explorer = False # or True to exclude pages of this type from Wagtail's explorer view
+    list_display = ['name']
 
 
 class ZabbixNetworksAdmin(ModelAdmin):
     model = ZabbixNetworks
     menu_label = 'Zabbix Networks'  # ditch this to use verbose_name_plural from model
-    menu_icon = 'group'  # change as required
+    menu_icon = 'cogs'  # change as required
     add_to_settings_menu = True  # or True to add your model to the Settings sub-menu
     exclude_from_explorer = False # or True to exclude pages of this type from Wagtail's explorer view
     list_display = ['name']
 
-    '''
-    panels = [
-        FieldPanel('name'),
-        FieldPanel('networks'),
-    ]
-    '''
 
+modeladmin_register(ZabbixConfigsAdmin)
 modeladmin_register(ZabbixNetworksAdmin)
 
