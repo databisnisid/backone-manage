3c4175e21f5717d4b2ee38427031bae09d4b4e54
proitlab <dedy.sutanto@proit.co.id>
2024-09-18 23:23:13 +0700

    Testing commit with LazyGit

--- webfilters/models.py
+++ webfilters/models.py
@@ -1,7 +1,11 @@
 from enum import unique
 import uuid
 from django.db import models
-from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist, ValidationError
+from django.core.exceptions import (
+    MultipleObjectsReturned,
+    ObjectDoesNotExist,
+    ValidationError,
+)
 from django.utils.translation import gettext as _
 from crum import get_current_user
 from accounts.models import User, Organizations
@@ -10,59 +14,67 @@ from members.models import Members
 
 # Create your models here.
 
+
 class WebFiltersOrg(models.Model):
     organization = models.OneToOneField(
         Organizations,
         on_delete=models.RESTRICT,
-        verbose_name=_('Organization'),
+        verbose_name=_("Organization"),
     )
 
     network = models.ForeignKey(
         Networks,
         on_delete=models.RESTRICT,
-        verbose_name=_('Network'),
+        verbose_name=_("Network"),
     )
 
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
-    
     class Meta:
-        db_table = 'webfilter_org'
-        verbose_name = 'Web Filter Network '
-        verbose_name_plural = 'Web Filters Network '
+        db_table = "webfilter_org"
+        verbose_name = "Web Filter Network "
+        verbose_name_plural = "Web Filters Network "
 
     def __str__(self):
-        return '{}'.format(self.organization)
+        return "{}".format(self.organization)
 
 
 class WebFilters(models.Model):
-    name = models.CharField(_('Name'), max_length=50, unique=True)
-    description = models.TextField(_('Description'), blank=True)
+    name = models.CharField(_("Name"), max_length=50, unique=True)
+    description = models.TextField(_("Description"), blank=True)
     uuid = models.UUIDField(editable=False, default=uuid.uuid4, unique=True)
-    domains = models.TextField(_('Black List'), help_text=_('List top domain to block'), default='atleastoneblacklistdomain.com')
-    domains_white = models.TextField(_('White List'), help_text=_('List top domain to allow'), default='atleastonewhitelistdomain.com')
-    is_default_block = models.BooleanField(_('Default Block All'), help_text=_('Check this for block all website as default'), default=False)
+    domains = models.TextField(
+        _("Black List"),
+        help_text=_("List top domain to block"),
+        default="atleastoneblacklistdomain.com",
+    )
+    domains_white = models.TextField(
+        _("White List"),
+        help_text=_("List top domain to allow"),
+        default="atleastonewhitelistdomain.com",
+    )
+    is_default_block = models.BooleanField(
+        _("Default Block All"),
+        help_text=_("Check this for block all website as default"),
+        default=False,
+    )
 
-    #network_id = models.CharField(_('Network ID'), max_length=50, blank=True, null=True)
+    # network_id = models.CharField(_('Network ID'), max_length=50, blank=True, null=True)
 
     user = models.ForeignKey(
-        User,
-        on_delete=models.SET_NULL,
-        verbose_name=_('User'),
-        blank=True,
-        null=True
+        User, on_delete=models.SET_NULL, verbose_name=_("User"), blank=True, null=True
     )
 
     organization = models.ForeignKey(
         Organizations,
         on_delete=models.SET_NULL,
-        verbose_name=_('Organization'),
+        verbose_name=_("Organization"),
         blank=True,
-        null=True
+        null=True,
     )
 
-    '''
+    """
     network = models.ForeignKey(
         Networks,
         on_delete=models.SET_NULL,
@@ -70,51 +82,55 @@ class WebFilters(models.Model):
         blank=True,
         null=True
     )
-    '''
+    """
 
     network = models.OneToOneField(
         Networks,
         on_delete=models.CASCADE,
-        verbose_name=_('Network'),
+        verbose_name=_("Network"),
         blank=True,
-        null=True
+        null=True,
     )
 
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
-
     class Meta:
-        db_table = 'webfilter'
-        verbose_name = 'Web Filter'
-        verbose_name_plural = 'Web Filters'
-
+        db_table = "webfilter"
+        verbose_name = "Web Filter"
+        verbose_name_plural = "Web Filters"
 
     def __str__(self):
-        return '{}'.format(self.name)
+        return "{}".format(self.name)
 
     def clean(self):
         current_user = get_current_user()
-        
+
         if not current_user.is_superuser:
             try:
-                network = WebFiltersOrg.objects.get(organization=current_user.organization)
+                network = WebFiltersOrg.objects.get(
+                    organization=current_user.organization
+                )
                 self.network = network.network
 
             except ObjectDoesNotExist or MultipleObjectsReturned:
-                if current_user.organization.features.is_webfilter and not current_user.organization.features.is_webfilter_multinet:
-                    raise ValidationError(_('Please setup Org<->Net correlation. Contact Administrator!'))
+                if (
+                    current_user.organization.features.is_webfilter
+                    and not current_user.organization.features.is_webfilter_multinet
+                ):
+                    raise ValidationError(
+                        _("Please setup Org<->Net correlation. Contact Administrator!")
+                    )
                 else:
                     pass
 
             if self.id is None:
                 try:
                     WebFilters.objects.get(network=self.network)
-                    raise ValidationError(_('IntegrityError: Duplicate Webfilter'))
+                    raise ValidationError(_("IntegrityError: Duplicate Webfilter"))
 
                 except ObjectDoesNotExist:
                     pass
-
 
     def save(self):
         self.domains = self.domains.lower()
@@ -133,64 +149,68 @@ class WebFiltersMembersList(Members):
         proxy = True
 
     def __str__(self):
-        return '%s' % self.name
+        return "%s" % self.name
 
 
 class WebFiltersMembers(models.Model):
     def limit_choices_to_member():
         current_user = get_current_user()
         if not current_user.is_superuser:
-            if current_user.organization.features.is_webfilter and not current_user.organization.features.is_webfilter_multinet:
+            if (
+                current_user.organization.features.is_webfilter
+                and not current_user.organization.features.is_webfilter_multinet
+            ):
                 try:
-                    network = WebFiltersOrg.objects.get(organization=current_user.organization)
-                    return {'network': network.network, 
-                            'is_waf': True }
+                    network = WebFiltersOrg.objects.get(
+                        organization=current_user.organization
+                    )
+                    return {"network": network.network, "is_waf": True}
 
                 except ObjectDoesNotExist or MultipleObjectsReturned:
-                    return {'organization': current_user.organization, 
-                            'is_waf': True }
+                    return {"organization": current_user.organization, "is_waf": True}
             else:
-                return {'organization': current_user.organization, 
-                        'is_waf': True }
+                return {"organization": current_user.organization, "is_waf": True}
         else:
-            return { 'is_waf': True }
+            return {"is_waf": True}
 
     def limit_choices_to_current_user():
         current_user = get_current_user()
         if not current_user.is_superuser:
-            if current_user.organization.features.is_webfilter and not current_user.organization.features.is_webfilter_multinet:
+            if (
+                current_user.organization.features.is_webfilter
+                and not current_user.organization.features.is_webfilter_multinet
+            ):
                 try:
-                    network = WebFiltersOrg.objects.get(organization=current_user.organization)
-                    return {'network': network.network }
+                    network = WebFiltersOrg.objects.get(
+                        organization=current_user.organization
+                    )
+                    return {"network": network.network}
 
                 except ObjectDoesNotExist or MultipleObjectsReturned:
-                    return {'organization': current_user.organization }
+                    return {"organization": current_user.organization}
             else:
-                return {'organization': current_user.organization }
+                return {"organization": current_user.organization}
         else:
             return {}
 
     member = models.OneToOneField(
-            Members,
-            on_delete=models.CASCADE,
-            limit_choices_to=limit_choices_to_member,
-            verbose_name=_('Member'),
-
-            )
+        Members,
+        on_delete=models.CASCADE,
+        limit_choices_to=limit_choices_to_member,
+        verbose_name=_("Member"),
+    )
 
     webfilter = models.ForeignKey(
-            WebFilters,
-            on_delete=models.CASCADE,
-            limit_choices_to=limit_choices_to_current_user,
-            verbose_name=_('WebFilter'),
-            )
+        WebFilters,
+        on_delete=models.CASCADE,
+        limit_choices_to=limit_choices_to_current_user,
+        verbose_name=_("WebFilter"),
+    )
 
     class Meta:
-        db_table = 'webfilter_member'
-        verbose_name = _('WebFilter Member')
-        verbose_name_plural = _('WebFilter Members')
-
+        db_table = "webfilter_member"
+        verbose_name = _("WebFilter Member")
+        verbose_name_plural = _("WebFilter Members")
 
     def __str__(self):
-        return '{}'.format(self.member)
-
+        return "{}".format(self.member)
