ddf4d26d82cda68b05734840d9ce4d2c7806c3ea
proitlab <dedy.sutanto@proit.co.id>
2024-09-19 19:56:09 +0700

    Clean up and formating

--- monitor/models.py
+++ monitor/models.py
@@ -1,37 +1,49 @@
 from django.core.validators import ValidationError
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
-from django.utils.html import format_html, format_html_join
+
+# from django.utils.html import format_html, format_html_join
+from django.utils.html import format_html_join
 from crum import get_current_user
 from accounts.models import User, Organizations
-from mqtt.models import Mqtt
-#from members.models import Members
+
+# from mqtt.models import Mqtt
+
+# from members.models import Members
 from networks.models import Networks
 from django.utils.translation import gettext as _
-from django.utils import timezone
-from config.utils import readable_timedelta_seconds
-from modelcluster.models import ClusterableModel
-from modelcluster.fields import ParentalKey
-from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel, FieldRowPanel, ObjectList
 
+# from django.utils import timezone
+# from config.utils import readable_timedelta_seconds
+# from modelcluster.models import ClusterableModel
+# from modelcluster.fields import ParentalKey
+"""
+from wagtail.admin.panels import (
+    FieldPanel,
+    InlinePanel,
+    MultiFieldPanel,
+    FieldRowPanel,
+    ObjectList,
+)
+"""
 
 DURATION_RED_ALERT = 3600
 
 
 class MonitorItems(models.Model):
-    name = models.CharField(_('Item'), max_length=50)
-    item_id = models.CharField(_('Item Keyword'), max_length=50, unique=True)
+    name = models.CharField(_("Item"), max_length=50)
+    item_id = models.CharField(_("Item Keyword"), max_length=50, unique=True)
 
     class Meta:
-        db_table = 'monitor_items'
-        verbose_name = _('Monitor Item')
-        verbose_name_plural = _('Monitor Items')
+        db_table = "monitor_items"
+        verbose_name = _("Monitor Item")
+        verbose_name_plural = _("Monitor Items")
 
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     def __str__(self):
-        return '{}'.format(self.name)
+        return "{}".format(self.name)
 
     def save(self):
         self.item_id = self.item_id.lower()
@@ -40,7 +52,7 @@ class MonitorItems(models.Model):
 
 
 class MonitorRules(models.Model):
-    '''
+    """
     def limit_choices_to_current_user():
         user = get_current_user()
         if not user.is_superuser:
@@ -50,56 +62,49 @@ class MonitorRules(models.Model):
                 return {'organization': user.organization}
         else:
             return {}
-    '''
-    name = models.CharField(_('Problem Name'), max_length=100)
-    item = models.ForeignKey(
-        MonitorItems,
-        on_delete=models.SET_NULL,
-        verbose_name=_('Item'),
-        null=True
+    """
 
+    name = models.CharField(_("Problem Name"), max_length=100)
+    item = models.ForeignKey(
+        MonitorItems, on_delete=models.SET_NULL, verbose_name=_("Item"), null=True
     )
     item_threshold = models.FloatField(
-        _('Threshold'),
-        default=0,
-        help_text='Float Value')
+        _("Threshold"), default=0, help_text="Float Value"
+    )
 
     user = models.ForeignKey(
-        User,
-        on_delete=models.SET_NULL,
-        verbose_name=_('Owner'),
-        null=True
+        User, on_delete=models.SET_NULL, verbose_name=_("Owner"), null=True
     )
     organization = models.ForeignKey(
         Organizations,
         on_delete=models.SET_NULL,
-        verbose_name=_('Organization'),
-        null=True
+        verbose_name=_("Organization"),
+        null=True,
     )
 
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     class Meta:
-        db_table = 'monitor_rules'
-        verbose_name = _('Monitor Rule')
-        verbose_name_plural = _('Monitor Rules')
+        db_table = "monitor_rules"
+        verbose_name = _("Monitor Rule")
+        verbose_name_plural = _("Monitor Rules")
 
     def __str__(self):
-        return '{}'.format(self.name)
+        return "{}".format(self.name)
 
     def save(self):
         if self.user is None:
             self.user = get_current_user()
 
-        #print('MonitorRules Model', self.user)
+        # print('MonitorRules Model', self.user)
 
         self.organization = self.user.organization
 
         return super(MonitorRules, self).save()
 
 
-'''
+"""
 class MemberProblemManagerUndone(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(
@@ -239,21 +244,22 @@ class MemberProblemsDone(MemberProblems):
 
     problem_duration_start_end.short_description = _('Problem')
 
-'''
+"""
+
+
 def validate_time(value):
     if value < 1 or value > 24:
-        raise ValidationError(
-            _("Range Time between 1 and 24!")
-        )
+        raise ValidationError(_("Range Time between 1 and 24!"))
+
 
 class OperationalTime(models.Model):
     def limit_choices_to_current_user():
         user = get_current_user()
         if not user.is_superuser:
             if user.organization.is_no_org:
-                return {'user': user}
+                return {"user": user}
             else:
-                return {'organization': user.organization}
+                return {"organization": user.organization}
         else:
             return {}
 
@@ -261,52 +267,48 @@ class OperationalTime(models.Model):
         Networks,
         on_delete=models.CASCADE,
         limit_choices_to=limit_choices_to_current_user,
-        verbose_name=_('Network'),
+        verbose_name=_("Network"),
     )
 
-    is_mon = models.BooleanField(_('Monday'), default=True)
-    mon_start = models.IntegerField(_('Start'), default=7, validators=[validate_time])
-    mon_end = models.IntegerField(_('End'), default=17, validators=[validate_time])
-    is_tue = models.BooleanField(_('Tuesday'), default=True)
-    tue_start = models.IntegerField(_('Start'), default=7, validators=[validate_time])
-    tue_end = models.IntegerField(_('End'), default=17, validators=[validate_time])
-    is_wed = models.BooleanField(_('Wednesday'), default=True)
-    wed_start = models.IntegerField(_('Start'), default=7, validators=[validate_time])
-    wed_end = models.IntegerField(_('End'), default=17, validators=[validate_time])
-    is_thu = models.BooleanField(_('Thursday'), default=True)
-    thu_start = models.IntegerField(_('Start'), default=7, validators=[validate_time])
-    thu_end = models.IntegerField(_('End'), default=17, validators=[validate_time])
-    is_fri = models.BooleanField(_('Friday'), default=True)
-    fri_start = models.IntegerField(_('Start'), default=7, validators=[validate_time])
-    fri_end = models.IntegerField(_('End'), default=17, validators=[validate_time])
-    is_sat = models.BooleanField(_('Saturday'), default=False)
-    sat_start = models.IntegerField(_('Start'), default=7, validators=[validate_time])
-    sat_end = models.IntegerField(_('End'), default=17, validators=[validate_time])
-    is_sun = models.BooleanField(_('Sunday'), default=False)
-    sun_start = models.IntegerField(_('Start'), default=7, validators=[validate_time])
-    sun_end = models.IntegerField(_('End'), default=17, validators=[validate_time])
+    is_mon = models.BooleanField(_("Monday"), default=True)
+    mon_start = models.IntegerField(_("Start"), default=7, validators=[validate_time])
+    mon_end = models.IntegerField(_("End"), default=17, validators=[validate_time])
+    is_tue = models.BooleanField(_("Tuesday"), default=True)
+    tue_start = models.IntegerField(_("Start"), default=7, validators=[validate_time])
+    tue_end = models.IntegerField(_("End"), default=17, validators=[validate_time])
+    is_wed = models.BooleanField(_("Wednesday"), default=True)
+    wed_start = models.IntegerField(_("Start"), default=7, validators=[validate_time])
+    wed_end = models.IntegerField(_("End"), default=17, validators=[validate_time])
+    is_thu = models.BooleanField(_("Thursday"), default=True)
+    thu_start = models.IntegerField(_("Start"), default=7, validators=[validate_time])
+    thu_end = models.IntegerField(_("End"), default=17, validators=[validate_time])
+    is_fri = models.BooleanField(_("Friday"), default=True)
+    fri_start = models.IntegerField(_("Start"), default=7, validators=[validate_time])
+    fri_end = models.IntegerField(_("End"), default=17, validators=[validate_time])
+    is_sat = models.BooleanField(_("Saturday"), default=False)
+    sat_start = models.IntegerField(_("Start"), default=7, validators=[validate_time])
+    sat_end = models.IntegerField(_("End"), default=17, validators=[validate_time])
+    is_sun = models.BooleanField(_("Sunday"), default=False)
+    sun_start = models.IntegerField(_("Start"), default=7, validators=[validate_time])
+    sun_end = models.IntegerField(_("End"), default=17, validators=[validate_time])
 
     user = models.ForeignKey(
-        User,
-        on_delete=models.SET_NULL,
-        verbose_name=_('Owner'),
-        null=True
+        User, on_delete=models.SET_NULL, verbose_name=_("Owner"), null=True
     )
     organization = models.ForeignKey(
         Organizations,
         on_delete=models.SET_NULL,
-        verbose_name=_('Organization'),
-        null=True
+        verbose_name=_("Organization"),
+        null=True,
     )
 
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     class Meta:
-        db_table = 'operational_time'
-        verbose_name = 'Operational Time'
-        verbose_name_plural = 'Operational Time'
-
+        db_table = "operational_time"
+        verbose_name = "Operational Time"
+        verbose_name_plural = "Operational Time"
 
     def clean(self):
         if self.user is None:
@@ -315,24 +317,42 @@ class OperationalTime(models.Model):
         self.organization = self.user.organization
 
         if self.mon_end < self.mon_start:
-            raise ValidationError({'mon_start':_('Start Time is bigger than End Time!')})
+            raise ValidationError(
+                {"mon_start": _("Start Time is bigger than End Time!")}
+            )
         if self.tue_end < self.tue_start:
-            raise ValidationError({'tue_start':_('Start Time is bigger than End Time!')})
+            raise ValidationError(
+                {"tue_start": _("Start Time is bigger than End Time!")}
+            )
         if self.wed_end < self.wed_start:
-            raise ValidationError({'wed_start':_('Start Time is bigger than End Time!')})
+            raise ValidationError(
+                {"wed_start": _("Start Time is bigger than End Time!")}
+            )
         if self.thu_end < self.thu_start:
-            raise ValidationError({'thu_start':_('Start Time is bigger than End Time!')})
+            raise ValidationError(
+                {"thu_start": _("Start Time is bigger than End Time!")}
+            )
         if self.fri_end < self.fri_start:
-            raise ValidationError({'fri_start':_('Start Time is bigger than End Time!')})
+            raise ValidationError(
+                {"fri_start": _("Start Time is bigger than End Time!")}
+            )
         if self.sat_end < self.sat_start:
-            raise ValidationError({'sat_start':_('Start Time is bigger than End Time!')})
+            raise ValidationError(
+                {"sat_start": _("Start Time is bigger than End Time!")}
+            )
         if self.sun_end < self.sun_start:
-            raise ValidationError({'sun_start':_('Start Time is bigger than End Time!')})
+            raise ValidationError(
+                {"sun_start": _("Start Time is bigger than End Time!")}
+            )
 
         if self.id is None:
             try:
-                OperationalTime.objects.get(network=self.network, organization=self.organization)
-                raise ValidationError({'network': _('Network already has Operation Time')})
+                OperationalTime.objects.get(
+                    network=self.network, organization=self.organization
+                )
+                raise ValidationError(
+                    {"network": _("Network already has Operation Time")}
+                )
             except ObjectDoesNotExist:
                 pass
 
@@ -348,21 +368,29 @@ class OperationalTime(models.Model):
         text = []
 
         if self.is_mon:
-            text.append('{}: {} - {}'.format(_('MONDAY'), self.mon_start, self.mon_end))
+            text.append("{}: {} - {}".format(_("MONDAY"), self.mon_start, self.mon_end))
         if self.is_tue:
-            text.append('{}: {} - {}'.format(_('TUESDAY'), self.tue_start, self.tue_end))
+            text.append(
+                "{}: {} - {}".format(_("TUESDAY"), self.tue_start, self.tue_end)
+            )
         if self.is_wed:
-            text.append('{}: {} - {}'.format(_('WEDNESDAY'), self.tue_start, self.tue_end))
+            text.append(
+                "{}: {} - {}".format(_("WEDNESDAY"), self.tue_start, self.tue_end)
+            )
         if self.is_thu:
-            text.append('{}: {} - {}'.format(_('THURSDAY'), self.thu_start, self.thu_end))
+            text.append(
+                "{}: {} - {}".format(_("THURSDAY"), self.thu_start, self.thu_end)
+            )
         if self.is_fri:
-            text.append('{}: {} - {}'.format(_('FRIDAY'), self.fri_start, self.fri_end))
+            text.append("{}: {} - {}".format(_("FRIDAY"), self.fri_start, self.fri_end))
         if self.is_sat:
-            text.append('{}: {} - {}'.format(_('SATURDAY'), self.sat_start, self.sat_end))
+            text.append(
+                "{}: {} - {}".format(_("SATURDAY"), self.sat_start, self.sat_end)
+            )
         if self.is_sun:
-            text.append('{}: {} - {}'.format(_('SUNDAY'), self.sun_start, self.sun_end))
+            text.append("{}: {} - {}".format(_("SUNDAY"), self.sun_start, self.sun_end))
 
-        #return format_html_join('<small>{}</small>', ', '.join(text))
-        return format_html_join('\n', '<small>{}<br /></small>', ((t,) for t in text))
-    get_operational_time.short_description = 'Operational Time'
+        # return format_html_join('<small>{}</small>', ', '.join(text))
+        return format_html_join("\n", "<small>{}<br /></small>", ((t,) for t in text))
 
+    get_operational_time.short_description = "Operational Time"
