7958a944279b03b889b40078b7303748d4436787
proitlab <dedy.sutanto@proit.co.id>
2025-05-15 16:04:49 +0700

    Remove Headscale, replace with Mqtt

--- config/db_routers.py
+++ config/db_routers.py
@@ -1,19 +1,21 @@
-class HeadscaleRouter:
+class MqttRouter:
 
-    route_app_labels = {"headscale"}
+    route_app_labels = {"mqtt"}
 
     def db_for_read(self, model, **hints):
-        if model._meta.app_label in self.route_app_labels:
-            return "headscale"
+        if model._meta.label_lower in self.route_app_labels:
+            print(model._meta.label_lower)
+            return "default_ramdisk"
         return None
 
     def db_for_write(self, model, **hints):
-        """
-        Writes always go to default.
-        """
+        if model._meta.label_lower in self.route_app_labels:
+            print(model._meta.label_lower)
+            return "default_ramdisk"
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label in self.route_app_labels:
-            return False
-        return True
+            print(app_label)
+            return db == "default_ramdisk"
+        return None
