7a03c084fa8916ddfb26421d1255b9cfdec693c0
proitlab <dedy.sutanto@proit.co.id>
2025-06-18 13:18:24 +0700

    redis ipinfo

---
+++ connectors/redis_ipinfo.py
@@ -0,0 +1,34 @@
+from django.core.exceptions import ObjectDoesNotExist
+import redis
+from redis.exceptions import TimeoutError
+import json
+from django.conf import settings
+
+
+def get_as_name(ipaddress: str) -> str:
+    result = ""
+    r = redis.Redis(
+        host=settings.MQTT_REDIS_HOST,
+        port=settings.MQTT_REDIS_PORT,
+        db=settings.MQTT_REDIS_DB,
+        socket_timeout=1,
+    )
+    key = f"{settings.IPINFO_LITE_PREFIX}:{ipaddress}"
+    try:
+        msg = r.get(key)
+    except TimeoutError:
+        msg = None
+
+    if msg:
+        try:
+            msg_string = msg.decode()
+            msg_json = json.loads(msg_string)
+            try:
+                result = msg_json["as_name"]
+            except IndexError or KeyError:
+                pass
+
+        except AttributeError:
+            pass
+
+    return result
