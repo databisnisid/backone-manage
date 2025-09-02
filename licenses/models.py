31a36090c309ca322f0268218a290eb880faa08d
proitlab <dedy.sutanto@proit.co.id>
2025-09-02 12:08:28 +0700

    Add LicenseFeatures in License

--- licenses/models.py
+++ licenses/models.py
@@ -10,59 +10,67 @@ from base64 import b64decode
 from rsa import PrivateKey, decrypt
 from config.utils import to_json, to_dictionary
 import json
-from accounts.models import Organizations
+from accounts.models import Organizations, LicenseFeatures
 from crum import get_current_user
 
 
 class Licenses(models.Model):
     def limit_choices_to_org():
-        return { 'is_no_org': False }
+        return {"is_no_org": False}
 
-    node_id = models.CharField(_('Node ID'), max_length=20, blank=True, null=True)
-    license_key = models.TextField(_('License Key'), blank=True, null=True)
-    license_string = models.TextField(_('License Code'), blank=True, null=True)
-    is_block_rule = models.BooleanField(_('Block Rule at Expired'), default=True)
+    node_id = models.CharField(_("Node ID"), max_length=20, blank=True, null=True)
+    license_key = models.TextField(_("License Key"), blank=True, null=True)
+    license_string = models.TextField(_("License Code"), blank=True, null=True)
+    is_block_rule = models.BooleanField(_("Block Rule at Expired"), default=True)
 
     organization = models.OneToOneField(
-            Organizations,
-            on_delete=models.SET_NULL,
-            blank=True,
-            null=True,
-            verbose_name=('Organization'),
-            limit_choices_to=limit_choices_to_org
-            )
+        Organizations,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name=("Organization"),
+        limit_choices_to=limit_choices_to_org,
+    )
+
+    license_features = models.OneToOneField(
+        LicenseFeatures,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name=("LicenseFeatures"),
+    )
 
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     class Meta:
-        db_table = 'licences'
-        verbose_name = 'License'
-        verbose_name_plural = 'Licenses'
+        db_table = "licences"
+        verbose_name = "License"
+        verbose_name_plural = "Licenses"
 
     def __str__(self):
-        return '%s' % self.node_id
+        return "%s" % self.node_id
 
     def clean(self):
         if self.organization is None:
-            raise ValidationError({'organization': _('Organization must be filled')})
+            raise ValidationError({"organization": _("Organization must be filled")})
 
     def save(self):
-        ''' Get first 11 characters (Docker supported) '''
+        """Get first 11 characters (Docker supported)"""
         node_id = hex(getnode())[:11]
-        #if self.node_id is None or self.node_id != node_id:
+        # if self.node_id is None or self.node_id != node_id:
         if self.node_id is None:
             self.node_id = node_id
 
         return super(Licenses, self).save()
-
 
     def get_organization_uuid(self):
         if self.organization:
             return self.organization.uuid
         else:
             return None
-    get_organization_uuid.short_description = _('UUID')
+
+    get_organization_uuid.short_description = _("UUID")
 
     def get_controller_token(self):
         if self.organization:
@@ -74,9 +82,11 @@ class Licenses(models.Model):
                 return None
         else:
             return None
-    get_controller_token.short_description = _('Token')
 
-    ''' Checking License '''
+    get_controller_token.short_description = _("Token")
+
+    """ Checking License """
+
     def check_license(self):
         license_status = False
         license_status_msg = []
@@ -91,9 +101,8 @@ class Licenses(models.Model):
             try:
                 lic_decode = b64decode(str(self.license_string))
             except:
-                ''' License Code Decode Error '''
-                license_status_msg.append('EC1105')
-
+                """License Code Decode Error"""
+                license_status_msg.append("EC1105")
 
             if lic_decode:
                 lic_key_enc = None
@@ -105,123 +114,125 @@ class Licenses(models.Model):
                     lic_key_enc = b64decode(str(self.license_key))
 
                 except:
-                    ''' License Key Decode Error '''
-                    license_status_msg.append('EC1107')
+                    """License Key Decode Error"""
+                    license_status_msg.append("EC1107")
 
                 if lic_key_enc:
                     try:
                         lic_key = PrivateKey.load_pkcs1(lic_key_enc)
-                    except: 
-                        ''' License Key Load Error '''
-                        license_status_msg.append('EC1108')
+                    except:
+                        """License Key Load Error"""
+                        license_status_msg.append("EC1108")
 
                 if lic_key:
                     try:
                         lic_decrypt = decrypt(lic_decode, lic_key).decode()
                         lic_json = to_json(lic_decrypt)
                     except:
-                        ''' License Key Decrypt Error '''
-                        license_status_msg.append('EC1109')
+                        """License Key Decrypt Error"""
+                        license_status_msg.append("EC1109")
 
-                #lic_key = PrivateKey.load_pkcs1(b64decode(str(self.license_key)))
-                #lic_decrypt = decrypt(lic_decode, lic_key).decode()
-                #lic_json = to_json(lic_decrypt)
+                # lic_key = PrivateKey.load_pkcs1(b64decode(str(self.license_key)))
+                # lic_decrypt = decrypt(lic_decode, lic_key).decode()
+                # lic_json = to_json(lic_decrypt)
 
                 if lic_json:
 
-                    node_id = ''
-                    organization_uuid = ''
-                    controller_token = ''
+                    node_id = ""
+                    organization_uuid = ""
+                    controller_token = ""
 
                     try:
-                        node_id = lic_json['node_id']
+                        node_id = lic_json["node_id"]
                     except:
-                        license_status_msg.append('EC1111')
+                        license_status_msg.append("EC1111")
 
-                    ''' Check Node ID '''
-                    ''' EC1101 - Node ID is not match '''
+                    """ Check Node ID """
+                    """ EC1101 - Node ID is not match """
                     status_node_id = True
                     if node_id != self.node_id:
                         status_node_id = True
-                        license_status_msg.append('EC1101')
+                        license_status_msg.append("EC1101")
 
                     try:
-                        organization_uuid = lic_json['organization_uuid']
+                        organization_uuid = lic_json["organization_uuid"]
                     except:
-                        license_status_msg.append('EC1112')
+                        license_status_msg.append("EC1112")
 
-                    ''' Check Organiation UUID '''
-                    ''' EC1102 - Organization UUID is not match '''
+                    """ Check Organiation UUID """
+                    """ EC1102 - Organization UUID is not match """
                     status_organization_uuid = True
                     if organization_uuid != str(self.get_organization_uuid()):
                         status_organization_uuid = False
-                        license_status_msg.append('EC1102')
+                        license_status_msg.append("EC1102")
 
                     try:
-                        controller_token = lic_json['controller_token']
+                        controller_token = lic_json["controller_token"]
                     except:
-                        license_status_msg.append('EC1113')
+                        license_status_msg.append("EC1113")
 
-                    ''' Check Token '''
-                    ''' EC1103 - Token is not match '''
+                    """ Check Token """
+                    """ EC1103 - Token is not match """
                     status_token = True
                     if controller_token != self.get_controller_token():
                         status_token = False
-                        license_status_msg.append('EC1103')
+                        license_status_msg.append("EC1103")
 
                     if status_node_id and status_organization_uuid and status_token:
                         try:
-                            valid_until = lic_json['valid_until']
-                            ''' Check Validity '''
-                            datetime_format = '%Y-%m-%d %H:%M:%S%z'
+                            valid_until = lic_json["valid_until"]
+                            """ Check Validity """
+                            datetime_format = "%Y-%m-%d %H:%M:%S%z"
                             try:
-                                license_valid_until = datetime.strptime(valid_until, datetime_format) 
+                                license_valid_until = datetime.strptime(
+                                    valid_until, datetime_format
+                                )
                                 current_time = timezone.now()
 
                                 if license_valid_until >= current_time:
                                     license_status = True
-                                    license_status_msg.append('VALID')
+                                    license_status_msg.append("VALID")
                                 else:
-                                    license_status_msg.append('EC1104')
+                                    license_status_msg.append("EC1104")
                             except:
-                                ''' Validity Error - Wrong date string '''
-                                license_status_msg.append('EC1114')
+                                """Validity Error - Wrong date string"""
+                                license_status_msg.append("EC1114")
 
                         except:
-                            ''' Validity Error - No valid_until '''
-                            license_status_msg.append('EC1114')
-                            
+                            """Validity Error - No valid_until"""
+                            license_status_msg.append("EC1114")
 
                 else:
-                    ''' if lic_json '''
-                    ''' JSON is None '''
-                    license_status_msg.append('EC1106')
+                    """if lic_json"""
+                    """ JSON is None """
+                    license_status_msg.append("EC1106")
 
-
-            #except ValueError:
+            # except ValueError:
             #    ''' License Key Error '''
             #    license_status_msg.append('EC1105')
 
         else:
-            ''' Initial Create License, licenses key and code are empty'''
-            license_status_msg.append('EC1100')
+            """Initial Create License, licenses key and code are empty"""
+            license_status_msg.append("EC1100")
 
-        license_msg = '; '.join(license_status_msg)
-            
+        license_msg = "; ".join(license_status_msg)
+
         return license_status, license_valid_until, license_msg
 
     def get_license_status(self):
         lic_status, lic_time, lic_msg = self.check_license()
         return lic_status
-    get_license_status.short_description = _('License Validity')
+
+    get_license_status.short_description = _("License Validity")
 
     def get_license_time(self):
         lic_status, lic_time, lic_msg = self.check_license()
         return lic_time
-    get_license_time.short_description = _('Valid Until')
+
+    get_license_time.short_description = _("Valid Until")
 
     def get_license_msg(self):
         lic_status, lic_time, lic_msg = self.check_license()
         return lic_msg
-    get_license_msg.short_description = _('License Status')
 
+    get_license_msg.short_description = _("License Status")
--- licenses/wagtail_hooks.py
+++ licenses/wagtail_hooks.py
@@ -1,6 +1,11 @@
 from wagtail.contrib.modeladmin.helpers import ButtonHelper
 from wagtail.contrib.modeladmin.options import (
-    ModelAdmin, ModelAdminGroup, ObjectList, PermissionHelper, modeladmin_register)
+    ModelAdmin,
+    ModelAdminGroup,
+    ObjectList,
+    PermissionHelper,
+    modeladmin_register,
+)
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel, FieldRowPanel
 from django.utils.translation import gettext_lazy as _
 from .models import Licenses
@@ -11,41 +16,45 @@ from django.urls import reverse
 
 class LicensesButtonHelper(ButtonHelper):
 
-    current_classnames = ['button button-small button-primary']
-
+    current_classnames = ["button button-small button-primary"]
 
     def json_button(self, obj):
-        text = _('Download License')
-        #obj_id = obj.id
-        button_url = reverse('json_download', args=[obj.id])
+        text = _("Download License")
+        # obj_id = obj.id
+        button_url = reverse("json_download", args=[obj.id])
 
         return {
-                'url': button_url,
-                'label': text,
-                'classname': self.finalise_classname(self.current_classnames),
-                'title': text,
-                }
+            "url": button_url,
+            "label": text,
+            "classname": self.finalise_classname(self.current_classnames),
+            "title": text,
+        }
 
-    def get_buttons_for_obj(self, obj, exclude=None, classnames_add=None, classnames_exclude=None):
+    def get_buttons_for_obj(
+        self, obj, exclude=None, classnames_add=None, classnames_exclude=None
+    ):
         buttons = super().get_buttons_for_obj(
             obj, exclude, classnames_add, classnames_exclude
         )
-        if 'json_button' not in (exclude or []):
+        if "json_button" not in (exclude or []):
             buttons.append(self.json_button(obj))
-                
 
         return buttons
 
+
 class LicensesPermissionHelper(PermissionHelper):
-    '''
+    """
 
     def user_can_list(self, user):
         return True
-    '''
-    
+    """
+
     def user_can_create(self, user):
         if user.is_superuser:
-            if Licenses.objects.all().count() < Organizations.objects.filter(is_no_org=False).count():
+            if (
+                Licenses.objects.all().count()
+                < Organizations.objects.filter(is_no_org=False).count()
+            ):
                 return True
             else:
                 return False
@@ -58,29 +67,39 @@ class LicensesPermissionHelper(PermissionHelper):
         else:
             return False
 
-    '''
+    """
     def user_can_edit_obj(self, user, obj):
         return False
-    '''
+    """
 
 
 class LicensesAdmin(ModelAdmin):
     model = Licenses
-    button_helper_class = LicensesButtonHelper   # Uncomment this to enable button
-    #inspect_view_enabled = True
-    menu_label = 'License'  # ditch this to use verbose_name_plural from model
-    menu_icon = 'key'  # change as required
+    button_helper_class = LicensesButtonHelper  # Uncomment this to enable button
+    # inspect_view_enabled = True
+    menu_label = "License"  # ditch this to use verbose_name_plural from model
+    menu_icon = "key"  # change as required
     add_to_settings_menu = True  # or True to add your model to the Settings sub-menu
-    exclude_from_explorer = False # or True to exclude pages of this type from Wagtail's explorer view
-    list_display = ('node_id', 'get_organization_uuid', 'get_controller_token', 'organization', 'get_license_time', 'get_license_msg',)
-    #search_fields = ('node_id', )
+    exclude_from_explorer = (
+        False  # or True to exclude pages of this type from Wagtail's explorer view
+    )
+    list_display = (
+        "node_id",
+        "get_organization_uuid",
+        "get_controller_token",
+        "organization",
+        "get_license_time",
+        "get_license_msg",
+    )
+    # search_fields = ('node_id', )
     permission_helper_class = LicensesPermissionHelper
 
     panels = [
-        FieldPanel('node_id', read_only=True),
-        FieldPanel('organization'),
+        FieldPanel("node_id", read_only=True),
+        FieldPanel("organization"),
+        FieldPanel("license_features", read_only=True),
     ]
-    '''
+    """
     panels = [
         FieldPanel('node_id', read_only=True),
         FieldPanel('organization'),
@@ -89,28 +108,37 @@ class LicensesAdmin(ModelAdmin):
             FieldPanel('license_string'),
             ], heading=_('License'))
     ]
-    '''
-
+    """
 
     def get_list_display(self, request):
         if request.user.is_superuser:
-            #list_display = ('node_id', 'get_organization_uuid', 'get_controller_token', 'organization', 'get_license_time', 'get_license_msg',)
-            list_display = ('node_id', 'get_organization_uuid', 'organization', 'get_license_time', 'get_license_msg',)
+            # list_display = ('node_id', 'get_organization_uuid', 'get_controller_token', 'organization', 'get_license_time', 'get_license_msg',)
+            list_display = (
+                "node_id",
+                "get_organization_uuid",
+                "organization",
+                "get_license_time",
+                "get_license_msg",
+            )
         else:
-            list_display = ('node_id', 'organization', 'get_license_time', 'get_license_msg',)
+            list_display = (
+                "node_id",
+                "organization",
+                "get_license_time",
+                "get_license_msg",
+            )
 
         return list_display
 
-
     def get_edit_handler(self):
         superuser_panels = [
-            FieldPanel('node_id', read_only=True),
-            FieldPanel('organization'),
+            FieldPanel("node_id", read_only=True),
+            FieldPanel("organization"),
         ]
 
         admin_panels = [
-            FieldPanel('node_id', read_only=True),
-            FieldPanel('organization', read_only=True),
+            FieldPanel("node_id", read_only=True),
+            FieldPanel("organization", read_only=True),
         ]
 
         current_user = get_current_user()
@@ -131,13 +159,13 @@ class LicensesAdmin(ModelAdmin):
 
         return qs
 
-    ''' Working INIT modeladmin '''
-    ''' Not Really Working. Need more test '''
-    '''
+    """ Working INIT modeladmin """
+    """ Not Really Working. Need more test """
+    """
     def __init__(self, *args, **kwargs):
         self.inspect_view_enabled = False
         super(LicensesAdmin, self).__init__(*args, **kwargs)
-    '''
+    """
+
 
 modeladmin_register(LicensesAdmin)
-
