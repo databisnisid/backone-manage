5447d979496c4b4fc78297b0d7ccaa8dcbf8db25
proitlab <dedy.sutanto@proit.co.id>
2025-11-12 16:20:52 +0700

    Update Custome Form User

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,17 +1,28 @@
 from django import forms
 from wagtail.users.forms import UserEditForm, UserCreationForm
-from .models import User, Organizations
+from .models import Organizations
 from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserEditForm(UserEditForm):
-    organization = forms.ModelChoiceField(queryset=Organizations.objects,
-                                          required=True,
-                                          disabled=True,
-                                          label=_("Organization"))
+    organization = forms.ModelChoiceField(
+        queryset=Organizations.objects,
+        required=True,
+        disabled=True,
+        label=_("Organization"),
+    )
+
+    class Meta(UserEditForm.Meta):
+        fields = UserEditForm.Meta.fields | {"organization"}
+        exclude = ["is_superuser"]
 
 
 class CustomUserCreationForm(UserCreationForm):
-    organization = forms.ModelChoiceField(queryset=Organizations.objects,
-                                          required=True,
-                                          label=_("Organization"))
+    organization = forms.ModelChoiceField(
+        queryset=Organizations.objects, required=True, label=_("Organization")
+    )
+
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields | {"organization"}
+        exclude = ["is_superuser"]
+
--- accounts/viewsets.py
+++ accounts/viewsets.py
@@ -1,10 +1,33 @@
-from wagtail.users.views.users import UserViewSet as WagtailUserViewSet
+from wagtail.users.views.users import (
+    UserViewSet as WagtailUserViewSet,
+    IndexView as WagtailIndexView,
+)
 
 from .forms import CustomUserCreationForm, CustomUserEditForm
 
 
+class CustomUserIndexView(WagtailIndexView):
+
+    def get_base_queryset(self):
+        queryset = super().get_base_queryset()
+        if not self.request.user.is_superuser:
+            queryset = queryset.exclude(is_superuser=True)
+
+        return queryset
+
+
 class UserViewSet(WagtailUserViewSet):
+
+    index_view_class = CustomUserIndexView
+
     def get_form_class(self, for_update=False):
         if for_update:
             return CustomUserEditForm
         return CustomUserCreationForm
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        # Apply your custom filtering here
+        queryset = queryset.exclude(is_superuser=True)
+        print(queryset)
+        return queryset
