b4b5c139e898fbcda80de61d0fdb1ebc2e640a70
proitlab <dedy.sutanto@proit.co.id>
2025-11-13 10:57:22 +0700

    Disable is_superuser when not superuser

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,7 @@ from wagtail.users.forms import UserEditForm, UserCreationForm
 from django.contrib.auth.models import Group
 from .models import Organizations
 from django.utils.translation import gettext_lazy as _
+from crum import get_current_user
 
 
 class CustomUserEditForm(UserEditForm):
@@ -13,6 +14,15 @@ class CustomUserEditForm(UserEditForm):
         label=_("Organization"),
     )
 
+    def __init__(self, *args, **kwargs):
+        editing_self = kwargs.pop("editing_self", False)
+        super().__init__(*args, **kwargs)
+        current_user = get_current_user()
+
+        if editing_self or not current_user.is_superuser:
+            del self.fields["is_active"]
+            del self.fields["is_superuser"]
+
     class Meta(UserEditForm.Meta):
         fields = UserEditForm.Meta.fields | {"organization"}
         # exclude = ["is_superuser"]
@@ -22,6 +32,13 @@ class CustomUserCreationForm(UserCreationForm):
     organization = forms.ModelChoiceField(
         queryset=Organizations.objects, required=True, label=_("Organization")
     )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        current_user = get_current_user()
+
+        if not current_user.is_superuser:
+            del self.fields["is_superuser"]
 
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields | {"organization"}
