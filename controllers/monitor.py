bc6b52d05d167bd4be530807ef60b72d87fbfc32
proitlab <dedy.sutanto@proit.co.id>
2025-11-21 14:38:04 +0700

    Check All Controller Status

---
+++ controllers/monitor.py
@@ -0,0 +1,28 @@
+import logging
+from .models import Controllers
+from .backend import Zerotier
+
+
+logger = logging.getLogger(__name__)
+
+
+def all_controllers_status():
+    controllers = Controllers.objects.all()
+
+    for controller in controllers:
+        zt = Zerotier(controller.uri, controller.token)
+        result = zt.status()
+
+        is_online = False
+        try:
+            is_online = result["online"]
+            is_error = False
+        except IndexError:
+            is_error = True
+
+        if is_error:
+            logger.error(f"Controller {controller.uri} is error!")
+        elif not is_online:
+            logger.warning(f"Controller {controller.uri} is OFFLINE")
+        else:
+            pass
