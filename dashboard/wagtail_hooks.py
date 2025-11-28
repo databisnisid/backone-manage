dfbbaa2f4ec2cbf08148c544f4289bb60417d83d
proitlab <dedy.sutanto@proit.co.id>
2025-11-28 20:33:20 +0700

    Remove insert_global_admin_css

--- dashboard/wagtail_hooks.py
+++ dashboard/wagtail_hooks.py
@@ -18,7 +18,8 @@ from wagtail.snippets.models import register_snippet
 from wagtail.snippets.views.snippets import SnippetViewSet, SnippetViewSetGroup
 from django.conf import settings
 from licenses.utils import is_license_valid
-from django.templatetags.static import static
+
+# from django.templatetags.static import static
 
 
 # @hooks.register("insert_global_admin_css", order=100)
@@ -28,12 +29,13 @@ from django.templatetags.static import static
 #    """
 #    return "<style>textarea {resize:vertical !important}</style>"
 
-
+"""
 @hooks.register("insert_global_admin_css")
 def global_admin_css():
     return format_html(
         '<link rel="stylesheet" href="{}">', static("dashboard/css/custom.css")
     )
+"""
 
 
 @hooks.register("construct_reports_menu", order=1)
