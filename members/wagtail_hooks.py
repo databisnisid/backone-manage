6eada2f3b4ddb2d07682085cd507f6b357b06734
proitlab <dedy.sutanto@proit.co.id>
2026-04-02 10:41:23 +0700

    Try to find the bug #4

--- members/wagtail_hooks.py
+++ members/wagtail_hooks.py
@@ -363,7 +363,7 @@ class MembersAdmin(ModelAdmin):
             "member_status",
             "model_release",
             "get_routes",
-            # "list_peers",
+            "list_peers",
         ]
         list_display_simple = [
             "name",
