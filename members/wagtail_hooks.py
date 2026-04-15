5b3c130c986adeed54109bebc1bb0ce33a09f938
proitlab <dedy.sutanto@proit.co.id>
2026-04-16 06:52:10 +0700

    Member Ordering reverse

--- members/wagtail_hooks.py
+++ members/wagtail_hooks.py
@@ -300,7 +300,7 @@ class MembersAdmin(ModelAdmin):
         False  # or True to exclude pages of this type from Wagtail's explorer view
     )
 
-    ordering = ("is_backone_online",)
+    ordering = ("-is_backone_online",)
 
     list_per_page = settings.MEMBERS_LIST_PER_PAGE
 
