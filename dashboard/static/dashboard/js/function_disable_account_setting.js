71755c7a720cfa191520b8e2877030b428168a4c
proitlab <dedy.sutanto@proit.co.id>
2026-04-16 18:21:54 +0700

    Further request to change

--- dashboard/static/dashboard/js/function_disable_account_setting.js
+++ dashboard/static/dashboard/js/function_disable_account_setting.js
@@ -65,6 +65,11 @@ document.onreadystatechange = function () {
       formToDelete.remove();
     }
 
+    var elementDisable = document.getElementById('id_username');
+    if (elementDisable) {
+      elementDisable.disabled = true;
+    }
+    /*
     var elementDisable = document.getElementById('id_name_email-first_name');
     if (elementDisable) {
       elementDisable.disabled = true;
@@ -77,6 +82,7 @@ document.onreadystatechange = function () {
     if (elementDisable) {
       elementDisable.disabled = true;
     }
+    */
   }
 }
 
